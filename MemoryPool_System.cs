348e5d5f784c936a4604fc07fbefa7e2927012c4
JinhoLee <dasoong15@naver.com>
2022-10-15 22:32:41 +0900

    코드 간소화
    코드 간소화

--- MemoryPool_System.cs
+++ MemoryPool_System.cs
@@ -7,98 +7,50 @@ using UnityEditor;
 public class MemoryPool_System : MonoBehaviour
 {
     //사용법 : 
-    //CallObject 함수로 오브젝트 불러오기.
+    //GetObj 함수로 오브젝트 불러오기.
     //사용하지 않는 오브젝트는 SetActive(false)
     ////////////////////////////////////////////////////////
     ///////////////     타입별 불러오기      ////////////////
     ////////////////////////////////////////////////////////
-    public GameObject CallObject(GameObject TargetGameObject)
+    public GameObject GetObj(GameObject TargetGameObject)
+    {
+        return CallObj(TargetGameObject, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0), null); ;
+    }
+    public GameObject GetObj(GameObject TargetGameObject, Vector3 position)
 
     {
-        return CallOObject_Active(TargetGameObject, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0), null); ;
+        return CallObj(TargetGameObject, position, Quaternion.Euler(0, 0, 0), null); ;
     }
-    public GameObject CallObject(GameObject TargetGameObject, Vector3 position)
-
+    public GameObject GetObj(GameObject TargetGameObject, Vector3 position, Quaternion rotation)
     {
-        return CallOObject_Active(TargetGameObject, position, Quaternion.Euler(0, 0, 0), null); ;
+        return CallObj(TargetGameObject, position, rotation, null);
     }
-    public GameObject CallObject(GameObject TargetGameObject, Vector3 position, Quaternion rotation)
+    public GameObject GetObj(GameObject TargetGameObject, Vector3 position, Quaternion rotation, Transform parent)
     {
-        return CallOObject_Active(TargetGameObject, position, rotation, null);
+        return CallObj(TargetGameObject, position, rotation, parent);
     }
-    public GameObject CallObject(GameObject TargetGameObject, Vector3 position, Quaternion rotation, Transform parent)
+    public void OffObj(GameObject obj) //오브젝트 끌때 사용
     {
-        return CallOObject_Active(TargetGameObject, position, rotation, parent);
+        obj.SetActive(false);
+        obj.transform.position = new Vector3(0, 0, 0);
+        obj.transform.rotation = Quaternion.identity;
+        obj.transform.localScale = new Vector3(1, 1, 1);
+        obj.transform.parent = ParentPool.transform;
     }
-    public List<GameObject> GetTargetObject(GameObject GameObject)
-    {
-        List<GameObject> AllObject = new List<GameObject>();
-        for (int i = 0; i < ObjectList.Count; i++)
-        {
-            List<GameObject> GetList = ObjectList[GameObject];
-
-            for (int j = 0; j < GetList.Count; j++)
-            {
-                AllObject.Add(GetList[j]); //모든 오브젝트에 추가
-            }
-        }
-
-        //오브젝트 없는지 확인 후 없을 경우 리스트에서 제거
-        for (var i = AllObject.Count - 1; i > -1; i--)
-        {
-            if (AllObject[i] == null)
-                AllObject.RemoveAt(i);
-        }
-
-        //리스트 중복 제거
-        AllObject = AllObject.Distinct().ToList();
-
-        return AllObject;
-    }
-    public List<GameObject> GetAllObject() //이 메모리풀 시스템에서 관리하는 모든 오브젝트를 가져옵니다. 게임 씬에 없는 오브젝트는 리스트에서 제거합니다.
-    {
-        List<GameObject> AllObject = new List<GameObject>();
-        for (int i = 0; i < ObjectList.Count; i++)
-        {
-            List<GameObject> GetList = ObjectList[ObjectList.Keys.ToArray()[i]];
-
-            for (int j = 0; j < GetList.Count; j++)
-            {
-                AllObject.Add(GetList[j]); //모든 오브젝트에 추가
-            }
-        }
-
-        //오브젝트 없는지 확인 후 없을 경우 리스트에서 제거
-        for (var i = AllObject.Count - 1; i > -1; i--)
-        {
-            if (AllObject[i] == null)
-                AllObject.RemoveAt(i);
-        }
-
-        //리스트 중복 제거
-        AllObject = AllObject.Distinct().ToList();
-
-        return AllObject;
-    }
-    public void RemoveTargetObject(GameObject GameObject) //이 메모리풀에서 해당 오브젝트를 사용하는 오브젝트를 제거 합니다.
-    {
-        foreach (var item in GetTargetObject(GameObject))
-        {
-            Destroy(item);
-        }
-    }
-    public void RemoveAllObject() //이 메모리풀에 존재 하는 모든 오브젝트를 제거 합니다.
+    public void ClearAll() //이 메모리풀에 존재 하는 모든 오브젝트를 제거 합니다.
     {
         foreach (var item in GetAllObject())
         {
             Destroy(item);
         }
     }
-
-
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     //싱글턴
+    Dictionary<GameObject, List<GameObject>> ObjectList = new Dictionary<GameObject, List<GameObject>>(); //실제로 쓰는 데이터
     public static MemoryPool_System instance = null; //게임 매니저 객체
+    GameObject ParentPool;
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     private void Awake()
     {
         if (instance == null) //게임 매니저 객체가 없을 때
@@ -109,31 +61,41 @@ public class MemoryPool_System : MonoBehaviour
         {
             Destroy(this.gameObject); //현재 객체를 제거
         }
+        ParentPool = new GameObject("MemoryPool_ObjsParent");
+        ParentPool.transform.parent = this.transform;
+        DontDestroyOnLoad(this.gameObject); //씬이 넘어가도 이 오브젝트는 유지한다.
     }
-
-    Dictionary<GameObject, List<GameObject>> ObjectList = new Dictionary<GameObject, List<GameObject>>(); //실제로 쓰는 데이터
-
-    /////////////////////////////////////////////////////////////
-    ///////////////     오브젝트 호출 시스템      ////////////////
-    /////////////////////////////////////////////////////////////
-    #region
-    //타입별 불러오기 처리
-    GameObject CallOObject_Active(GameObject TargetGameObject, Vector3 position, Quaternion rotation, Transform parent)
+    public List<GameObject> GetAllObject() //이 메모리풀 시스템에서 관리하는 모든 오브젝트를 가져옵니다. 게임 씬에 없는 오브젝트는 리스트에서 제거합니다.
+    {
+        List<GameObject> AllObject = new List<GameObject>();
+        for (int i = 0; i < ObjectList.Count; i++)
+        {
+            List<GameObject> GetList = ObjectList[ObjectList.Keys.ToArray()[i]];
+            foreach (var item in GetList)
+            {
+                AllObject.Add(item);
+            }
+        }
+        return AllObject;
+    }
+    GameObject CallObj(GameObject TargetGameObject, Vector3 position, Quaternion rotation, Transform parent)
     {
         if (ObjectList.ContainsKey(TargetGameObject) == false) //리스트 자체가 없을 경우
         {
-            return CreateNewObjectList(TargetGameObject, position, rotation, parent); //리스트도 새로 생성 하고 오브젝트도 새로 생성해서 추가
+            List<GameObject> TargetObjectList = new List<GameObject>();
+            ObjectList[TargetGameObject] = TargetObjectList;
+            GameObject obj = InstantiateObj(TargetGameObject, position, rotation, parent);
+            return obj; //리스트도 새로 생성 하고 오브젝트도 새로 생성해서 추가
         }
         else //있을 경우
         {
-            GameObject GetObject = SearchListActive(TargetGameObject);
-            if (GetObject == null) //리스트가 있는데 오브젝트가 없을 경우
+            GameObject GetObject = FindObj_ActiveFalse(TargetGameObject);
+            if (GetObject == null)
             {
-                return AddTargetDictionalyList(TargetGameObject, position, rotation, parent); //해당 리스트에 새로운놈 추가 한 뒤에 리턴 받음
+                return InstantiateObj(TargetGameObject, position, rotation, parent); //해당 리스트에 새로운놈 추가 한 뒤에 리턴 받음
             }
             else //리스트도 있고 비활성 오브젝트도 있을 경우
             {
-                //오브젝트 위치, 회전값 적용 후 켜기
                 GetObject.transform.position = position;
                 GetObject.transform.rotation = rotation;
                 GetObject.transform.parent = parent;
@@ -142,38 +104,19 @@ public class MemoryPool_System : MonoBehaviour
             }
         }
     }
-    GameObject CreateNewObjectList(GameObject CreateTargetObject, Vector3 position, Quaternion rotation, Transform parent) //새로운 딕셔너리 리스트 정보 추가
-    {
-        //새로운 리스트 구성
-        List<GameObject> TargetObjectList = new List<GameObject>();
-        GameObject NewObject = Instantiate(CreateTargetObject, position, rotation, parent);
-        //NewObject.SetActive(false); //오브젝트 끄기
-        TargetObjectList.Add(NewObject); //리스트 추가 한뒤
-        ObjectList.Add(CreateTargetObject, TargetObjectList); //딕셔너리 정보 추가 후
-        return NewObject; //선택된 오브젝트 바로 보내기
-    }
-    GameObject AddTargetDictionalyList(GameObject OriginalObject, Vector3 position, Quaternion rotation, Transform parent) //딕셔너리 정보가 이미 있는 곳에 새로운 오브젝트 추가
+    GameObject InstantiateObj(GameObject OriginalObject, Vector3 position, Quaternion rotation, Transform parent) 
     {
         GameObject NewObject = Instantiate(OriginalObject, position, rotation, parent); //새로운 오브젝트 생성
         ObjectList[OriginalObject].Add(NewObject); //리스트에 이 데이터 추가
         return NewObject; //선택된 오브젝트 바로 보내기
     }
-    GameObject SearchListActive(GameObject TargetGameObject) //리스트 안에 꺼져 있는 오브젝트 있는지 확인
+    GameObject FindObj_ActiveFalse(GameObject GetObj) //리스트 안에 꺼져 있는 오브젝트 있는지 확인
     {
-        for (int i = 0; i < ObjectList[TargetGameObject].Count; i++)
+        List<GameObject> ObjList = ObjectList[GetObj];
+        foreach (var item in ObjList)
         {
-            if (ObjectList[TargetGameObject][i] == null)
-            {
-                ObjectList[TargetGameObject].Remove(ObjectList[TargetGameObject][i]); //만약에 대상이 씬에 없을 경우 리스트에서 제거
-            }
-            else if (ObjectList[TargetGameObject][i].activeSelf == false)
-            {
-                return ObjectList[TargetGameObject][i];
-            }
+            if(item.activeSelf == false) return item;
         }
         return null;
     }
-    #endregion
-
 }
-
